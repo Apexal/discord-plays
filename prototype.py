e4d2fe40a8403121a9aad1e92015c973dd7d8c2f
Frank Matranga <thefrankmatranga@gmail.com>
2017-11-13 19:56:25 -0500

    Listen toggle

--- prototype.py
+++ prototype.py
@@ -1,29 +1,53 @@
 import discord
 import asyncio
 import pyautogui
-import time
+import os
 
 client = discord.Client()
 
+control_channel_id = '222576355362078731'
+owner_user_id = '152621041976344577' # My Discord ID
+
+class Player:
+    def __init__(self):
+        self.listening = True
+
+    def handle_input(self, input, is_owner=False):
+        if input == 'stop listening' and is_owner:
+            self.listening = False
+            print('Stopped listening...')
+        elif input == 'start listening' and is_owner:
+            self.listening = True
+            print('Started listening...')
+        elif self.listening:
+            for text in input.split(' '):
+                text = text.lower()
+
+                self.keyboard(text)
+                print(text)
+
+    def keyboard(self, key):
+        key = key.lower()
+
+        if key in ['up', 'down', 'left', 'right', 'enter', 'return', 'shift', 'escape']:
+            pyautogui.press(key)
+        else:
+            pyautogui.typewrite(key, interval=0.2)
+
 @client.event
-async def on_ready():
-		print('Logged in as', client.user.name, client.user.id)
-		print('https://discordapp.com/oauth2/authorize?&client_id=379677652518174727&scope=bot+&permissions=8')
-
-def keyboard(key):
-		key = key.lower()
-
-		if key in ['up', 'down', 'left', 'right']:
-				pyautogui.press(key)
-		else:
-				pyautogui.typewrite(key[0])
+async def on_error(event):
+    raise
 
 @client.event
 async def on_message(message):
-		keyboard(message.content)
-		
-		#await client.send_message(message.channel, 'Typed it!')
+    if not message.author.bot:
+        player.handle_input(message.content, message.author.id == owner_user_id)
 
-import os
+@client.event
+async def on_ready():
+    print('Logged in as', client.user.name, client.user.id)
+    print('https://discordapp.com/oauth2/authorize?&client_id=379677652518174727&scope=bot+&permissions=8')
+
+player = Player()
 token = os.environ['token']
 client.run(token)
